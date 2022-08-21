7e2ace7d7ab1658d23ebf3451475d4622fc53f0a
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-21 23:41:35 +0700

    Update code

--- 02-03/02_Function_q03/answer.h
+++ 02-03/02_Function_q03/answer.h
@@ -7,6 +7,19 @@ using namespace std;
 bool completeNum(int N)
 {
     // TODO
+    int i=1, sum=0, n=N;
+    while(i<n){
+        if (n%i ==0){
+            sum +=i;
+            ++i;
+        }
+    }
+    if (sum==n){
+        return true;
+    }
+    else {
+        return false;
+    }
     
 }
 /// END  <STUDENT ANSWER>
