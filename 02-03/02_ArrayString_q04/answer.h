298e70fde53805a003236faeb0f191ad70529e3c
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-08-22 13:11:48 +0700

    Update answer.h

--- 02-03/02_ArrayString_q04/answer.h
+++ 02-03/02_ArrayString_q04/answer.h
@@ -10,10 +10,10 @@ void process(char str[], char outstr[]) {
     int i, n = strlen(str);
     for (i=0; i<n; i++){
         if (str[i]>=97 && str[i]<=122){
-            outstr = &str[i]-32;
+            outstr[i] = str[i]-32;
         } 
         else{
-            outstr = &str[i];
+            outstr[i]= str[i];
         }
     }
 }
