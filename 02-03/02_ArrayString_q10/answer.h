94ab2bc800785748e0ad10154d472d58a5581bf0
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 16:50:10 +0700

    Update answer.h

--- 02-03/02_ArrayString_q10/answer.h
+++ 02-03/02_ArrayString_q10/answer.h
@@ -6,9 +6,11 @@ using namespace std;
 bool isSymmetric(int arr[][1000], int row, int col) {
     //TODO
     int i, j;
-    for (i=0; i<row; i++){
-        for (j=0; j<col; j++){
- 
+    for (i=0; i<row; ++i){
+        for (j=0; j<col; ++j){
+            if (arr[i][j] != arr[j][i]){
+                return false;
+            }
         }
     }
 }
