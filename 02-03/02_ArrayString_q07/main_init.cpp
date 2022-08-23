843812a51d21fd89e8f66ab5dde56a78161d3989
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 16:36:30 +0700

    Update main_init.cpp

--- 02-03/02_ArrayString_q07/main_init.cpp
+++ 02-03/02_ArrayString_q07/main_init.cpp
@@ -9,9 +9,15 @@ int main()
     // codeCheck(CHECKED_FILENAME, NO_IGNORED_LINES);
     
     // BEGIN  <TEST CODE>
-    string s1 = "Truong Dai Hoc Bach Khoa.";
-    string s2 = "a";
-    findAllIndex(s1, s2);
+    int index=0, temp=0, string=s1.find(s2, index);
+    temp = string;
+    cout << temp;
+    while(string != string::npos){
+        string = s1.find(s2, string+1);
+        if (string != -1){
+            cout << " " << string;
+        }
+    }
     /// END  <TEST CODE>
 
     return 0;
