a6d47eafbb67c5a0dbed76af14ec3d79354c109b
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 18:36:32 +0700

    Update answer.h

--- 02-03/02_Struct_q04/answer.h
+++ 02-03/02_Struct_q04/answer.h
@@ -15,5 +15,46 @@ struct SCP {
 
 string convertToString(SCP obj) {
     // Student answer
+    string resultStr = "Item #: SCP-";
+    int countIdDigits = 0;
+    char* idStr;
+    int cpyId = obj.id;
+    string objClassStr[] = {"Safe", "Euclid", "Keter"};
+
+    for (int i = cpyId; i != 0; i /= 10)
+    {
+        countIdDigits++;
+    }
+
+    if(countIdDigits < 3)
+    {
+        countIdDigits = 3;
+    }
+
+    idStr = new char[countIdDigits];
+    for (int i = countIdDigits - 1; i >= 0;i--)
+    {
+        idStr[i] = cpyId % 10 + 48;
+        cpyId /= 10;
+    }
+
+    resultStr += idStr;
+
+    resultStr += "\nObject Class: ";
+    resultStr += objClassStr[obj.objClass];
+
+    resultStr += "\nSpecial Containment Procedures: ";
+    resultStr += obj.speConProcedures;
+
+    resultStr += "\nDescription: ";
+    resultStr += obj.description;
+    
+    for(int i = 0; i < obj.numAddendums; i++)
+    {
+        resultStr += "\n";
+        resultStr += obj.addendums[i];
+    }
+
+    return resultStr;
 }
 /// END  <STUDENT ANSWER>
