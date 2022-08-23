0cf4854477ce2fbbf0c1481668c2b84ff44cb3b5
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 16:58:56 +0700

    Update answer.h

--- 02-03/02_ArrayString_q06/answer.h
+++ 02-03/02_ArrayString_q06/answer.h
@@ -5,9 +5,13 @@ using namespace std;
 
 void cutString(string s, int index){
     // TODO
+    void cutString(string s, int index){
+    // TODO
     int i, n = s.length();
-    string result = "";
+    string result= "";
     // for (i=0; i<n; +i){ 
-    result = substr(index, n);
+    result = s.substr(index, n-index-1);
+    cout << result;
     // }
+}
 }
