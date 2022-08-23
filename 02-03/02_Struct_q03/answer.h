6d2a33598af7dbbf951230083e526cc000139238
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 18:33:28 +0700

    Update answer.h

--- 02-03/02_Struct_q03/answer.h
+++ 02-03/02_Struct_q03/answer.h
@@ -15,5 +15,13 @@ struct SCP {
 
 SCP createSCP(int id, int objClass, string speConProcedures, string description, string* addendums, int numAddendums) {
     // Student answer
+    SCP newScp;
+    newScp.id = id;
+    newScp.objClass = objClass;
+    newScp.speConProcedures = speConProcedures;
+    newScp.description = description;
+    newScp.addendums = addendums;
+    newScp.numAddendums = numAddendums;
+    return newScp;
 }
 /// END  <STUDENT ANSWER>
