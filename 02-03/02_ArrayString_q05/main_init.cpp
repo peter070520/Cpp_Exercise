7888f350ef0281534a5eb0c3b2423aac72738252
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 17:15:23 +0700

    Update main_init.cpp

--- 02-03/02_ArrayString_q05/main_init.cpp
+++ 02-03/02_ArrayString_q05/main_init.cpp
@@ -8,11 +8,23 @@ int main()
 {
     // codeCheck(CHECKED_FILENAME, NO_IGNORED_LINES);
     
-    // BEGIN  <TEST CODE>
-    char str[] = "doAn VAN *(()hau @!#$" ;
-    char *outstr = new char[strlen(str) + 1];
-    process(str, outstr);
-    cout << outstr;
+    // BEGIN  <TEST CODE>    
+    int i=0, j=0, n=strlen(str);
+    for (i=0; i<n; ++i){
+        if ((str[i]>='a' && str[i] <='z') || (str[i] >= 'A' && str[i] <='Z') || str[i] == ' '){
+            outstr[j] = str[i];
+            ++j;
+        } 
+    }
+    for (i=0; i<j; ++i){
+        outstr[i] = tolower(outstr[i]);
+    }
+    outstr[0] = toupper(str[0]);
+    for (i=1; i<j; ++i){
+        if (outstr[i] == ' '){
+            outstr[i+1] = toupper(outstr[i+1]);
+        }
+    }
     /// END  <TEST CODE>
 
     return 0;
