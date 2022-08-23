6c8e56886b238d603cac1833d64720a4019db932
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 18:37:47 +0700

    Update answer.h

--- 02-03/02_Struct_q05/answer.h
+++ 02-03/02_Struct_q05/answer.h
@@ -15,6 +15,18 @@ struct SCP {
 
 SCP* cloneSCP(SCP* original) {
     // Student answer
-    
+    SCP *newScp = new SCP;
+    newScp->id = original->id;
+    newScp->objClass = original->objClass;
+    newScp->speConProcedures = original->speConProcedures;
+    newScp->description = original->description;
+    newScp->numAddendums = original->numAddendums;
+    newScp->addendums = new string[newScp->numAddendums];
+    for(int i = 0; i < original->numAddendums; i++)
+    {
+        newScp->addendums[i] = original->addendums[i];
+    }
+
+    return newScp;  
 }
 /// END  <STUDENT ANSWER>
