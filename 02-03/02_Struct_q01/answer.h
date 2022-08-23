af4760b71ecd64e7181afc45765666fd30a6edb2
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 18:29:33 +0700

    Update answer.h

--- 02-03/02_Struct_q01/answer.h
+++ 02-03/02_Struct_q01/answer.h
@@ -6,6 +6,11 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 struct SCP {
     // TODO: Student answer
-    
+    int id;
+    int objClass;
+    string speConProcedures;
+    string description;
+    string* addendums;
+    int numAddendums;
 };
 /// END  <STUDENT ANSWER>
