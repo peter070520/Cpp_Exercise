b4b2ffbb06b2c0eb1ca6fe74f94de6f93540029a
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 13:32:24 +0700

    Update answer.h

--- 02-03/02_Pointer_q05/answer.h
+++ 02-03/02_Pointer_q05/answer.h
@@ -6,29 +6,32 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 void insertRow(int**& matrix, int r, int c, int* rowArr, int row) {
     // TODO
-    int i, j, temp=0, rowAbove=0;
-    
-    
-    if (row < r){
-        // matrix[r] = new int[c];
-        for (i=0; i<c; ++i){
-            *(*(matrix+r)+i) = *(rowArr+i);
+    if (row == r)
+    {
+        matrix[row] = new int[c];
+        for (int i = 0; i < c; i++)
+        {
+            *(*(matrix + row) + i) = *(rowArr + i);
         }
-        for (i=r; i>row; --i){
-            for (j=0; i<c; ++j){
-                rowAbove = i-1;
-                temp = *(*(matrix +rowAbove) +j);
-                *(*(matrix+rowAbove)+j) = *(*(matrix+i)+j);
-                *(*(matrix+i)+j) = temp;
+    }
+    else if (row < r)
+    {
+        matrix[r] = new int[c];
+        for (int i = 0; i < c; i++)
+        {
+            *(*(matrix + r) + i) = *(rowArr + i);
+        }
+        for (int i = r; i > row; i--)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                int temp;
+                int rowbefore = i - 1;
+                temp = *(*(matrix + rowbefore) + j);
+                *(*(matrix + rowbefore) + j) = *(*(matrix + i) + j);
+                *(*(matrix + i) + j) = temp;
             }
         }
     }
-    else if(row ==r){
-        // matrix[row] = new int[c];
-        for (i=0; i<c; ++i){
-            *(*(matrix+row)+i) = *(rowArr+i);
-        }
-    }
-    
 }
 /// END  <STUDENT ANSWER>
