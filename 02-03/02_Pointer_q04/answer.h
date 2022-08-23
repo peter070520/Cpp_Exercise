1af5898cff2c8795a670c9689cf2564b299e9130
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 11:31:13 +0700

    Update answer.h

--- 02-03/02_Pointer_q04/answer.h
+++ 02-03/02_Pointer_q04/answer.h
@@ -7,6 +7,15 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 void deleteMatrix(int**& matrix, int r) {
     // TODO:
+    int i;
+    for (i=0; i<r; ++i){
+        delete [] matrix[i];
+    }
+    delete[] matrix;
+    matrix = NULL;
+    if (matrix == NULL) {
+        cout << "SUCCESSFULLY";
+    }
 
 }
 /// END  <STUDENT ANSWER>
