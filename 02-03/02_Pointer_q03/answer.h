d866987d52631d28dccaeed346e8dbec9f4aadcb
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 12:21:20 +0700

    Update answer.h

--- 02-03/02_Pointer_q03/answer.h
+++ 02-03/02_Pointer_q03/answer.h
@@ -6,13 +6,11 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 void reverse(int *ptr, int n)
 {
-    int i=0, reverse=0, j=n-1;
-    while (i<j){
-        reverse = *(ptr+i);
-        *(ptr+i) = *(ptr+j);
-        *(ptr+j) = reverse;
-        ++i;
-        --j;
+    int i=0, reverse=0;
+    for (i=0; i<n/2; ++i){
+        reverse = *(ptr +i);
+        *(ptr +i) = *(ptr +n -i -1);
+        *(ptr +n-i-1) = reverse;
     }
 }
 /// END  <STUDENT ANSWER>
