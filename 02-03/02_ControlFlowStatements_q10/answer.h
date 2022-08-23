900ccc40a58b16eec79a634b08a4af93cb04e9d7
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 18:26:59 +0700

    Update answer.h

--- 02-03/02_ControlFlowStatements_q10/answer.h
+++ 02-03/02_ControlFlowStatements_q10/answer.h
@@ -3,41 +3,25 @@
 
 using namespace std;
 
-int Fib(int n){
-    n = Fib(n-1)+Fib(n-2);
-    return n;
-}
 
 void tempMain() {
     /// BEGIN  <STUDENT ANSWER>
-    int n; //, num1=0, num2=1, temp=0;
+    int i, n, a=0, b=1, c=1;
     cin >> n;
-
-    if ((n>=1) && (n<=90)){
-        if (n==1) {
-            n = 0;
-            cout << n;
-        }
-        else if(n==2){
-            n=1;
-            cout << n;
-        }
-        else {
-            n= Fib(n);
-            cout << n;
-        }
-        // for(int i; i <=n; i++){
-        //     if(n==1 || n||2){
-        //         n=1;
-        //         cout << n;
-        //         continue;
-        //     }
-        //     temp = num1 + num2;
-        //     num1 = num2;
-        //     num2 = temp;
-        // }
-        // cout << temp;
+    do{
+        cin >> n;
     }
-    cout << n;
+    while (n<1 || n>90);
+    if (n==0 || n==1){
+        c = n;
+    }
+    else{
+        for (i=3; i<n; ++i){
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+    cout << c;
     /// END  <STUDENT ANSWER>
 }
