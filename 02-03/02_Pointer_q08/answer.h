0091ec75f0b7cc09c90b6810c2293cae158495ee
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-23 14:50:58 +0700

    Update answer.h

--- 02-03/02_Pointer_q08/answer.h
+++ 02-03/02_Pointer_q08/answer.h
@@ -6,6 +6,17 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 int** transposeMatrix(int** matrix, int r, int c) {
     // TODO:
-    
+    int i, j;
+    int** newArr = new int*[c];
+    for (i=0; i<c; ++i){
+        newArr[i] = new int[r];
+    }
+
+    for (i=0; i<c; ++i){
+        for(j = 0; j < r; ++j){
+            newArr[i][j] = matrix[j][i];
+        }
+    }
+    return newArr;    
 }
 /// END  <STUDENT ANSWER>
