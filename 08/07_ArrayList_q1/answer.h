4fac0a14105588913cd0c5f84934b805fd1d2148
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-27 17:29:55 +0700

    update code

--- 08/07_ArrayList_q1/answer.h
+++ 08/07_ArrayList_q1/answer.h
@@ -7,5 +7,14 @@ using namespace std;
 int buyCar(int* nums, int length, int k) {
 
 	//TODO
-	
+	sort(nums, nums+length);
+	int i=0, total=0;
+	for (i<length; total+nums[i]<=k; ++i){
+		total += nums[i];
+	}
+	// while (total+nums[i]<=k && i<length){
+	// 	total+=nums[i];
+	// 	++i;
+	// }
+	return i;
 }
