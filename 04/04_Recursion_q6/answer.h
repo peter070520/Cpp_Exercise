402bec246043fb2dd1abf4fe922dec84d374f60c
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-27 17:24:21 +0700

    update code exercise

--- 04/04_Recursion_q6/answer.h
+++ 04/04_Recursion_q6/answer.h
@@ -15,14 +15,14 @@ void printPattern(int n)
         printPattern(n - 5);
     }
 	cout << n << " ";
-	remove_spaces(n);
+	// remove_spaces(n);
 	// (string) n.earse(remove(n.begin(), n.end(), ' '), n.end());
 }
-string remove_spaces(const string &s)
-{
-  int last = s.size() - 1;
-  while (last >= 0 && s[last] == ' ')
-    --last;
-  return s.substr(0, last + 1);
-}
+// string remove_spaces(const string &s)
+// {
+//   int last = s.size() - 1;
+//   while (last >= 0 && s[last] == ' ')
+//     --last;
+//   return s.substr(0, last + 1);
+// }
 /// END  <STUDENT ANSWER>
