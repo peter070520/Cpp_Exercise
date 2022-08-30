0c6ddceac529ee17737f327bdb05f284fa925ca7
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-30 13:14:14 +0700

    Update answer.h

--- 05-06-07/05_BasicOOP_q02/answer.h
+++ 05-06-07/05_BasicOOP_q02/answer.h
@@ -4,5 +4,28 @@
 using namespace std;
 
 /// BEGIN  <STUDENT ANSWER>
+class Room
+{
+private:
+    int length, breadth, height;
 
+public:
+    int calculateArea(int length, int breadth, int height);
+    int calculateVolume(int length, int breadth, int height);
+    Room(int length, int breadth, int height)
+    {
+        this->length = length;
+        this->breadth = breadth;
+        this->height = height;
+    }
+};
+
+int Room::calculateArea(int length, int breadth, int height)
+{
+    return length * height;
+}
+int Room::calculateVolume(int length, int breadth, int height)
+{
+    return length * breadth * height;
+}
 /// END  <STUDENT ANSWER>
