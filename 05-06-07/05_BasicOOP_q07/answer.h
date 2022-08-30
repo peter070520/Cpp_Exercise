7166fb4417517f7115a1e78d1e728583d27e2c44
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-30 16:13:57 +0700

    Update answer.h

--- 05-06-07/05_BasicOOP_q07/answer.h
+++ 05-06-07/05_BasicOOP_q07/answer.h
@@ -5,6 +5,31 @@ using namespace std;
 
 /// BEGIN  <STUDENT ANSWER>
 // TODO
+class Book
+{
+public:
+    Book(const char *name);
+    ~Book();
+    void display();
 
+private:
+    char *name;
+};
+
+Book::Book(const char *name)
+{
+    this->name = new char(strlen(name));
+    strcpy(this->name, name);
+}
+
+Book::~Book()
+{
+    delete[] this->name;
+}
+
+void Book::display()
+{
+    cout << "Book: " << name;
+}
 
 /// END  <STUDENT ANSWER>
