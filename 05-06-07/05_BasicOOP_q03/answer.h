353d14e83c2eaf0c71cfe85eb07ed0b7c271278d
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-30 13:21:22 +0700

    Update answer.h

--- 05-06-07/05_BasicOOP_q03/answer.h
+++ 05-06-07/05_BasicOOP_q03/answer.h
@@ -6,31 +6,45 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 class ClockType
 {
-   public:
-      ClockType(int, int, int); //constructor with parameters
-      ClockType(); //default constructor
-      
-      void printTime() const;
+public:
+    ClockType(int, int, int); // constructor with parameters
+    ClockType();              // default constructor
 
-   private:
-      int hr;
-      int min;
-      int sec;
+    void printTime() const;
+
+private:
+    int hr;
+    int min;
+    int sec;
 };
 
-void ClockType::printTime() const { 
-    if (hr < 10) 
-        cout << "0"; 
-    cout << hr << ":"; 
-    if (min < 10) 
-        cout << "0"; 
-    cout << min << ":"; 
-    if (sec < 10) 
-        cout << "0"; 
+ClockType::ClockType(int hr, int min, int sec)
+{
+    this->hr = hr;
+    this->min = min;
+    this->sec = sec;
+}
+
+ClockType::ClockType()
+{
+    this->hr = 0;
+    this->min = 0;
+    this->sec = 0;
+}
+
+void ClockType::printTime() const
+{
+    if (hr < 10)
+        cout << "0";
+    cout << hr << ":";
+    if (min < 10)
+        cout << "0";
+    cout << min << ":";
+    if (sec < 10)
+        cout << "0";
     cout << sec;
 }
 
-//TODO
-
+// TODO
 
 /// END  <STUDENT ANSWER>
