c3b515b3db49d8c4f17dd03df19a215b2c771918
SouthernSemicolon <cobaohieu@gmail.com>
2022-08-30 15:41:05 +0700

    Update answer.h

--- 05-06-07/05_BasicOOP_q06/answer.h
+++ 05-06-07/05_BasicOOP_q06/answer.h
@@ -5,6 +5,56 @@ using namespace std;
 
 /// BEGIN  <STUDENT ANSWER>
 // TODO
+class Coordinate
+{
+public:
+    Coordinate(float, float);
+    void setX(float);
+    void setY(float);
+    float getX();
+    float getY();
+    float distanceTo(const Coordinate &other);
 
+private:
+    float x;
+    float y;
+};
+Coordinate::Coordinate(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+void Coordinate::setX(float x)
+{
+    this->x = x;
+}
+
+void Coordinate::setY(float y)
+{
+    this->y = y;
+}
+
+// void Coordinate::setXY(float x, float x){
+//     x=this->setX;
+//     y= this->setY;
+// }
+
+float Coordinate::getX()
+{
+    return x;
+}
+
+float Coordinate::getY()
+{
+    return y;
+}
+
+float Coordinate::distanceTo(const Coordinate &other)
+{
+    float result = 0;
+    result = abs(this->x - other.x) + abs(this->y - other.y);
+    return result;
+}
 
 /// END  <STUDENT ANSWER>
