bf4ebaa621a554260b641630130013bf6b9d2c04
SouthernSemicolon <cobaohieu@gmail.com>
2022-09-04 15:00:59 +0700

    Update answer.h

--- 08/07_ArrayList_q2/answer.h
+++ 08/07_ArrayList_q2/answer.h
@@ -5,8 +5,21 @@ using namespace std;
 
 	/// BEGIN  <STUDENT ANSWER>
 bool consecutiveOnes(vector<int>& nums) {
-    //Implement content in function  here
-	
+    //Implement content in function  here    
+    int i=0, meet=0;
+    if(nums.size()==0){
+        return true;
+    }
+    
+    for(i=1; i<nums.size(); ++i){
+        if (nums[i]==1 && meet==0){
+            meet=1;
+        }
+        else if (nums[i]==1 && meet==1 && nums[i-1]!=1){
+            return false;
+        }
+    }
+    return true;
 }
 
 	
