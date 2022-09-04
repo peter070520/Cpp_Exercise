f210aabb22349ea81214d2157821cc5f8bec7122
SouthernSemicolon <cobaohieu@gmail.com>
2022-09-04 21:30:02 +0700

    update code

--- 08/07_ArrayList_q4/answer.h
+++ 08/07_ArrayList_q4/answer.h
@@ -6,11 +6,18 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 int longestSublist(vector<string>& words) {
     // STUDENT ANSWER
-    int i=0, maxLength=0, size=words.size();
-    for (i=0; i<size; ++i){
-        if(words[i].size()>maxLength){
-            maxLength=words[i].size();
+    int i=0, maxLength=0, currentLength=0, size=words.size();
+    char currentLetter='\0';
+    for (i=1; i<size; ++i){
+        if(!currentLetter || currentLetter!=words[i][0]){
+            maxLength=max(maxLength, currentLength);
+            currentLetter=words[i][0];
+            currentLength=1;
+        }
+        else {
+            currentLength+=1;
         }
     }
+    return max(maxLength, currentLength);
 }
 /// END  <STUDENT ANSWER>
